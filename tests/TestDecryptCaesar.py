b63900158a2874c76f3ac273599224b65cfe8407
j6r <32883148+j6r@users.noreply.github.com>
2018-03-23 21:20:34 -0700

    Refactoring test cases

--- tests/TestDecryptCaesar.py
+++ tests/TestDecryptCaesar.py
@@ -1,6 +1,17 @@
 from unittest import TestCase
 from decrypt import DecryptCaesar
 
+test_cases = [
+    ("english_simple",
+     "Nokb Onsdyb, Drsc sc dro Jynskm czokusxq. Sx kxcgob dy iyeb kcusxq pyb wybo nodksvc klyed dro qyyn dswoc S rkfo rkn sx Fkvvoty, S crkvv lo fobi rkzzi dy cezzvi ofox wybo wkdobskv. Li dro gki, kbo dro zyvsmo rkfosxq k qyyn dswo gsdr dro myno? Sp xyd, dovv drow dy mroob ez; grox droi ny mbkmu sd droi gsvv rkfo wo.",
+     "DEAR EDITOR, THIS IS THE ZODIAC SPEAKING. IN ANSWER TO YOUR ASKING FOR MORE DETAILS ABOUT THE GOOD TIMES I HAVE HAD IN VALLEJO, I SHALL BE VERY HAPPY TO SUPPLY EVEN MORE MATERIAL. BY THE WAY, ARE THE POLICE HAVEING A GOOD TIME WITH THE CODE? IF NOT, TELL THEM TO CHEER UP; WHEN THEY DO CRACK IT THEY WILL HAVE ME."),
+    ("english_garbage_word",
+    "aopz pz alea aoha jvuahpuz h nhyihnl zhsrkqmszrhkqm dvyk.",
+     "THIS IS TEXT THAT CONTAINS A GARBAGE SALKDJFLSKADJF WORD.")
+]
+
+test_context = {"test": "test"}
+
 
 class TestCaesar(TestCase):
     """
@@ -8,14 +19,6 @@ class TestCaesar(TestCase):
     """
 
     def test_lambda_handler(self):
-
-        inputEvent = {
-            "userId": "testuser",
-            "inputText": "Nokb Onsdyb, Drsc sc dro Jynskm czokusxq. Sx kxcgob dy iyeb kcusxq pyb wybo nodksvc klyed dro qyyn dswoc S rkfo rkn sx Fkvvoty, S crkvv lo fobi rkzzi dy cezzvi ofox wybo wkdobskv. Li dro gki, kbo dro zyvsmo rkfosxq k qyyn dswo gsdr dro myno? Sp xyd, dovv drow dy mroob ez; grox droi ny mbkmu sd droi gsvv rkfo wo."
-        }
-
-        inputContext = {"test": "test"}
-        expectedResult = "DEAR EDITOR, THIS IS THE ZODIAC SPEAKING. IN ANSWER TO YOUR ASKING FOR MORE DETAILS ABOUT THE GOOD TIMES I HAVE HAD IN VALLEJO, I SHALL BE VERY HAPPY TO SUPPLY EVEN MORE MATERIAL. BY THE WAY, ARE THE POLICE HAVEING A GOOD TIME WITH THE CODE? IF NOT, TELL THEM TO CHEER UP; WHEN THEY DO CRACK IT THEY WILL HAVE ME."
-
-        result = DecryptCaesar.lambda_handler(inputEvent, inputContext)
-        self.assertEqual(expectedResult, result['decrypted'][1])
+        for test_case in test_cases:
+            result = DecryptCaesar.lambda_handler({"inputText": test_case[1]}, test_context)
+        self.assertEqual(test_case[2], result['decrypted'][1])
